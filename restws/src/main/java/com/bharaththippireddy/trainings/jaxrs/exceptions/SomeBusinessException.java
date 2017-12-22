5d55dfe01d527de0327ab495485ff06ee63489e5
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2017-12-22 16:57:39 -0500

    Added structure for error handling

---
+++ restws/src/main/java/com/bharaththippireddy/trainings/jaxrs/exceptions/SomeBusinessException.java
@@ -0,0 +1,7 @@
+package com.bharaththippireddy.trainings.jaxrs.exceptions;
+
+public class SomeBusinessException extends RuntimeException {
+	public SomeBusinessException(String message) {
+		super(message);
+	}
+}
