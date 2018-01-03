1ac1961d5999fa9efb43af6c97e06d8984ce829b
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-01-03 03:49:02 -0500

    Basic check service client

---
+++ restwsasync/src/main/java/com/bharaththippireddy/trainings/jaxrs/client/CheckProcessor.java
@@ -0,0 +1,27 @@
+package com.bharaththippireddy.trainings.jaxrs.client;
+
+import com.bharaththippireddy.trainings.jaxrs.ChecksList;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class CheckProcessor {
+
+    public static void main(String[] args) {
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080/services/checkservice/checks");
+
+        AsyncInvoker asyncInvoker = target.request().async();
+        Future<Boolean> future = asyncInvoker.post(Entity.entity(new ChecksList(), MediaType.APPLICATION_XML), Boolean.class);
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
