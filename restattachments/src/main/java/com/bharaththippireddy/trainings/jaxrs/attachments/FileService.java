99889614967d66f1370760c5942a9180cbe72ba0
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-01-04 10:10:06 -0500

    Added upload and download methods

--- restattachments/src/main/java/com/bharaththippireddy/trainings/jaxrs/attachments/FileService.java
+++ restattachments/src/main/java/com/bharaththippireddy/trainings/jaxrs/attachments/FileService.java
@@ -1,15 +1,46 @@
 package com.bharaththippireddy.trainings.jaxrs.attachments;
 
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Paths;
+import java.util.List;
+
 
 public class FileService {
 
-	private static final String FILE_PATH = "/Users/bharaththippireddy/Desktop/uploads/uploaded.jpg";
+	private static final String FILE_PATH = "/Users/Ubnik/Desktop/uploads/uploaded.jpg";
+
+	@POST
+	@Path("/upload")
+	public void upload(List<Attachment> attachments) throws IOException{
+		for (Attachment attachment: attachments) {
+			InputStream inputStream = attachment.getDataHandler().getInputStream();
+			copyFile(inputStream);
+		}
+	}
+
+	@GET
+	@Path("/download")
+	public Response download(){
+		java.nio.file.Path path = Paths.get(FILE_PATH);
+		//File file = new File(FILE_PATH);
+
+		Response.ResponseBuilder responseBuilder = Response.ok(path.toFile());
+		responseBuilder.header("Content-Disposition", "attachment;filename=downloaded.jpg");
+
+		return responseBuilder.build();
+	}
 
 	private void copyFile(InputStream inputStream) throws FileNotFoundException, IOException {
 		OutputStream out = null;
